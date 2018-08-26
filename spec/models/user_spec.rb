cdee7bda0d5ea87afed56c1fd2f0218babef8290
sambineroloc <sambineroloc@gmail.com>
2018-08-26 14:24:48 -0400

    Create rails and spec helper, get first test passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+    user = User.new(
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    expect(user).to be_valid
+  end
+
+  it 'is not valid without a title'
+  it 'is not valid without a description'
+  it 'is not valid without a start_date'
+  it 'is not valid without a end_date'
 end
