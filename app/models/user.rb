9d9bc03b71ea22c0d83b8d1b7cccf5ab4d5e67ee
sambineroloc <sambineroloc@gmail.com>
2018-08-26 14:28:49 -0400

    Refactor tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 # Model representing the user
 class User < ApplicationRecord
   has_secure_password
+
+  validates_presence_of :username, :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'is valid with valid attributes' do
-    user = User.new(
+  subject do
+    described_class.new(
+      username: 'bobo',
+      email: 'bobo@gmail.com',
       password: 'password',
       password_confirmation: 'password'
     )
-    expect(user).to be_valid
   end
 
-  it 'is not valid without a title'
-  it 'is not valid without a description'
-  it 'is not valid without a start_date'
-  it 'is not valid without a end_date'
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a username' do
+    subject.username = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a email'
 end
