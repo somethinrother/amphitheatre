6f549e480001c69b872a4290cd6d355682899937
sambineroloc <sambineroloc@gmail.com>
2018-08-26 23:11:10 -0400

    Reformatting tests

--- spec/models/blue_book_spec.rb
+++ spec/models/blue_book_spec.rb
@@ -5,22 +5,38 @@ RSpec.describe BlueBook, type: :model do
     create(:blue_book)
   end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  describe 'when saving' do
+    context 'with valid attributes' do
+      it 'saves successfully' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'with missing attributes' do
+      it 'is not valid without a chapter' do
+        subject.chapter = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid without a title' do
+        subject.title = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid without a body' do
+        subject.body = nil
+        expect(subject).to_not be_valid
+      end
+    end
   end
 
-  it 'is not valid without a chapter' do
-    subject.chapter = nil
-    expect(subject).to_not be_valid
-  end
+  describe 'when observing relationships' do
+    it 'belongs to a chapter' do
+      expect(subject.chapter).to eq(Chapter.first)
+    end
 
-  it 'is not valid without a title' do
-    subject.title = nil
-    expect(subject).to_not be_valid
-  end
-
-  it 'is not valid without a body' do
-    subject.body = nil
-    expect(subject).to_not be_valid
+    it 'belongs to a character' do
+      expect(subject.character).to eq(Character.first)
+    end
   end
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -5,41 +5,49 @@ RSpec.describe Campaign, type: :model do
     create(:campaign)
   end
 
-  it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+  describe 'when saving' do
+    context 'with valid attributes' do
+      it 'saves successfully' do
+        expect(subject).to be_valid
+      end
+    end
+
+    context 'with missing attributes' do
+      it 'is not valid without a user' do
+        subject.user = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid without a title' do
+        subject.title = nil
+        expect(subject).to_not be_valid
+      end
+
+      it 'is not valid without a description' do
+        subject.description = nil
+        expect(subject).to_not be_valid
+      end
+    end
   end
 
-  it 'is not valid without a user' do
-    subject.user = nil
-    expect(subject).to_not be_valid
-  end
+  describe 'when observing relationships' do
+    it 'belongs to a user' do
+      expect(subject.user).to eq(User.first)
+    end
 
-  it 'is not valid without a title' do
-    subject.title = nil
-    expect(subject).to_not be_valid
-  end
+    it 'can own a setting detail' do
+      create(:setting_detail, campaign: subject)
+      expect(subject.setting_details.count).to eq(1)
+    end
 
-  it 'is not valid without a description' do
-    subject.description = nil
-    expect(subject).to_not be_valid
-  end
+    it 'can own a chapter' do
+      create(:chapter, campaign: subject)
+      expect(subject.chapters.count).to eq(1)
+    end
 
-  it 'belongs to a user' do
-    expect(subject.user).to eq(User.first)
-  end
-
-  it 'can own a setting detail' do
-    create(:setting_detail, campaign: subject)
-    expect(subject.setting_details.count).to eq(1)
-  end
-
-  it 'can own a chapter' do
-    create(:chapter, campaign: subject)
-    expect(subject.chapters.count).to eq(1)
-  end
-
-  it 'can own a character' do
-    create(:character, campaign: subject)
-    expect(subject.characters.count).to eq(1)
+    it 'can own a character' do
+      create(:character, campaign: subject)
+      expect(subject.characters.count).to eq(1)
+    end
   end
 end
