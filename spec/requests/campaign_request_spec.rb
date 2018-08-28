72abb89467f2520a81711442425751af0b4cbc14
sambineroloc <sambineroloc@gmail.com>
2018-08-28 12:23:18 -0400

    Make test more blunt for easier rewrites and debuggin

--- spec/requests/campaign_request_spec.rb
+++ spec/requests/campaign_request_spec.rb
@@ -21,14 +21,14 @@ RSpec.describe 'Campaign requests', :type => :request do
 
   describe 'POST /campaigns' do
     let(:user) { create(:user, id: 1) }
-    let(:json_helper) { Helpers::JSON.new('campaign', { title: 'Wheel of Time', description: 'Great book' }) }
-    let(:headers) { json_helper.json_headers }
-    let(:valid_attributes) { json_helper.build_test_attributes }
 
     context 'when the request is valid' do
 
       it "can create a campaign" do
         user
+        json_helper = Helpers::JSON.new('campaign', { title: 'Wheel of Time', description: 'Great book' })
+        headers = json_helper.json_headers
+        valid_attributes = json_helper.build_test_attributes
         post campaigns_path, params: valid_attributes, headers: headers
         hash_body = nil
         expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
