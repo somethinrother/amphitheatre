a807245a9f9bb8a37a0f8dadcec29e1867097ce5
James Neilson <james@quandl.com>
2018-09-10 10:04:57 -0700

    Update user request spec

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'User requests', :type => :request do
+  hash_body = nil
 
-  describe 'GET /users/:id.json' do
-
-    context 'when the request is valid' do
-
-      it "can render a user's information" do
+  describe 'GET requests to' do
+    context '/users/:id.json' do
+      it 'returns the information of one user' do
         user = create(:user)
         get "/users/#{user.id}.json"
-        hash_body = nil
+
         expect {
           hash_body = JSON.parse(response.body).with_indifferent_access
         }.not_to raise_exception
-        expect(hash_body['data']['attributes']).to match_array([["email", user.email], ["username", user.username]])
-        expect(hash_body['data']['attributes']).to match({
-          'username': user.username,
-          'email': user.email
-        })
+        expect(hash_body['data']['attributes']).to match(
+          {
+            'email': user.email,
+            'username': user.username
+          }
+        )
+        expect(response.status).to eq(200)
       end
     end
   end
@@ -26,12 +27,54 @@ RSpec.describe 'User requests', :type => :request do
     context 'when the request is valid' do
       it "can create a user" do
         json_helper = Helpers::JSON.new('user')
-        headers = json_helper.json_headers
-        successful_post = json_helper.successful_post
-        post users_path, params: successful_post, headers: headers
-        hash_body = nil
-        expect { hash_body = JSON.parse(response.body).with_indifferent_access }.not_to raise_exception
+        post users_path,
+          params: json_helper.successful_post,
+          headers: json_helper.json_headers
+
+        expect {
+          hash_body = JSON.parse(response.body).with_indifferent_access
+        }.not_to raise_exception
         expect(response.status).to eq(201)
+      end
+    end
+  end
+
+  describe 'PUT requests' do
+    let(:user) { create(:user) }
+
+    context 'when the request is valid' do
+      it 'updates the user' do
+        json_helper = Helpers::JSON.new('user')
+        put user_path(user),
+          params: json_helper.successful_put(user.id),
+          headers: json_helper.json_headers
+
+        expect {
+          hash_body = JSON.parse(response.body).with_indifferent_access
+        }.not_to raise_exception
+        data = hash_body["data"]
+        attributes = data["attributes"]
+        expect(data["id"]).to eq(user.id.to_s)
+        expect(attributes["email"]).to eq('email@gmail.com')
+        expect(attributes["username"]).to eq('username')
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+
+  describe 'DELETE requests' do
+    let(:user) { create(:user) }
+
+    context 'when the request is valid' do
+      it 'deletes the user' do
+        user
+        expect(User.all.length).to eq(1)
+        json_helper = Helpers::JSON.new('user')
+        headers = json_helper.json_headers
+        delete user_path(user), headers: headers
+
+        expect(User.all.length).to eq(0)
+        expect(response.status).to eq(204)
       end
     end
   end
