db1d59e20d3bad5992ce043c7633a233bbd28cd6
James Neilson <james@quandl.com>
2018-11-11 02:11:09 -0500

    Create associations and basic validations for locations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,9 +2,10 @@
 class Campaign < ApplicationRecord
   belongs_to :user
 
-  has_many :setting_details
   has_many :chapters
   has_many :characters
+  has_many :locations
+  has_many :setting_details
 
   validates_presence_of %i[title description]
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
+# A location within a given campaign
 class Location < ApplicationRecord
+  belongs_to :campaign
+
+  validates_presence_of %i[name description]
 end
