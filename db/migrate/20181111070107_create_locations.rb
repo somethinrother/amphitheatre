74e150fbc7f6b44e6a7f429f2860187fe2aedc0b
James Neilson <james@quandl.com>
2018-11-11 02:08:12 -0500

    Run migration

--- db/migrate/20181111070107_create_locations.rb
+++ db/migrate/20181111070107_create_locations.rb
@@ -1,6 +1,9 @@
 class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
+      t.integer :campaign_id
+      t.string :name
+      t.text :description
 
       t.timestamps
     end
