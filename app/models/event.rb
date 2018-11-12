a703214100b88ab75ed8144f5547c6def0dbcc13
James Neilson <james@quandl.com>
2018-11-11 22:05:11 -0500

    Update relationships between chapters and events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
+# An event. Takes place within a chapter and can reference characters
+# and/or locations through an array of ids
 class Event < ApplicationRecord
+  belongs_to :chapter
+
+  validates_presence_of %i[title description]
 end
