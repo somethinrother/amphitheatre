eabadbbcb97f14d476d8ba564c6e02a4dce738c9
James Neilson <james@quandl.com>
2018-11-11 22:13:32 -0500

    Get location_ids and character_ids to serialize to arrays

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 # An event. Takes place within a chapter and can reference characters
 # and/or locations through an array of ids
 class Event < ApplicationRecord
+  serialize :character_ids, Array
+  serialize :location_ids, Array
+
   belongs_to :chapter
 
   validates_presence_of %i[title description]
