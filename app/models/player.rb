f9131f4e326c495fbc729c6a71d7bf938a8066cb
somethinrother <jamesneilson4@gmail.com>
2019-07-01 18:20:22 -0400

    Add last validation, tests are failing

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,10 @@
 class Player < ApplicationRecord
+  ROLES = %w[GM PC].freeze
+
   belongs_to :campaign
   belongs_to :user
 
   validates_presence_of %i[role]
+  validates :role, inclusion: { in: ROLES,
+    message: "%{value} is not valid. Role must be either GM or PC" }
 end
