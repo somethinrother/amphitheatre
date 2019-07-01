b7d8f8f4a9b8fce797356c040f84f8108243c99d
somethinrother <jamesneilson4@gmail.com>
2019-07-01 17:11:55 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_025052) do
+ActiveRecord::Schema.define(version: 2018_11_11_070107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "blue_books", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.string "reward"
-    t.integer "chapter_id"
-    t.integer "character_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "campaigns", force: :cascade do |t|
     t.integer "user_id"
@@ -37,27 +27,6 @@ ActiveRecord::Schema.define(version: 2018_11_12_025052) do
     t.string "title"
     t.text "description"
     t.integer "campaign_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "characters", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "campaign_id"
-    t.string "name"
-    t.text "description"
-    t.string "pc_class"
-    t.integer "level"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "events", force: :cascade do |t|
-    t.integer "chapter_id"
-    t.text "location_ids"
-    t.text "character_ids"
-    t.string "title"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -109,14 +78,6 @@ ActiveRecord::Schema.define(version: 2018_11_12_025052) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
-  end
-
-  create_table "setting_details", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.integer "campaign_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
