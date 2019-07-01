0fa95d9e3be8e516864d66fcfe132c26fa8bbf5e
somethinrother <jamesneilson4@gmail.com>
2019-07-01 18:10:45 -0400

    Add content needed for correct player requests

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  belongs_to :campaign
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :campaigns
+  has_many :players
 
   validates_presence_of %i[username email]
 end
