b70bd9fc0f3a2c9d458e1a10fdedfb2ff868f29e
somethinrother <jamesneilson4@gmail.com>
2019-07-01 16:58:21 -0400

    Remove character related files

--- app/models/character.rb
+++
@@ -1,7 +0,0 @@
-# A character, played by either a user or a gm
-class Character < ApplicationRecord
-  belongs_to :user
-  belongs_to :campaign
-
-  validates_presence_of %i[name description pc_class level]
-end
