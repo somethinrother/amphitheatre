6980a8bd48af0d04dc2b3ce7f11518451625f89a
somethinrother <jamesneilson4@gmail.com>
2019-07-01 15:54:41 -0400

    Remove stage from amphitheatre

--- stage/mirage/factories/user.js
+++
@@ -1,7 +0,0 @@
-import { Factory } from 'ember-cli-mirage';
-
-export default Factory.extend({
-  username(i) { return `superduder${i}`; },
-  email(i) { return `superduder${i}@email.com` },
-  password(i) { return `difhblsdkghlwfdsl${i}` }
-});
